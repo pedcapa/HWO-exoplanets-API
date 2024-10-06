2a295f793089f017ca80165492136149cfbc71e6
Pedro Castañeda <catbox.py@gmail.com>
2024-10-06 01:34:15 -0600

    Endpoint: Show all the exoplanets sorted by ESI (Earth Similarity Index)

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ except FileNotFoundError:
 
 df = df.replace({np.nan: None})
 
-# show the complete csv
+# show all the exoplanets with all the fields
 @app.get("/exoplanets/all")
 async def get_all_exoplanets():
   return [{"n": df.shape[0]}, df.to_dict(orient='records')]
+
+# show all the exoplanets sorted by esi -> descending order
+@app.get("/exoplanets/esi")
+async def get_exoplanets_esi():
+  filtered_df = df[['pl_name', 'P_ESI']].dropna(subset=['P_ESI'])
+  if filtered_df.empty:
+    raise HTTPException(status_code=404, detail="No exoplanets with a valid 'P_ESI' value were found.")
+  data = [{"n": len(filtered_df)}, filtered_df.sort_values(by='P_ESI', ascending=False).to_dict(orient='records')]
+  return data
